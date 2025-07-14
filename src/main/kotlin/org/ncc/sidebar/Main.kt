4845af9282b3fcc7f3a33ca6202f6624115340f1
NatJerry <xiaomajunqwq123@outlook.com>
2025-07-14 15:08:32 +0800

    Init Commit

---
+++ src/main/kotlin/org/ncc/sidebar/Main.kt
@@ -0,0 +1,14 @@
+package org.ncc.sidebar
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
